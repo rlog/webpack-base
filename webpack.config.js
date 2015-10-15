6621f38414e2df59d15e0db189bab5d4b598cbfa
邓国梁 <guoliang.deng@ele.me>
2015-10-15 18:12:16 +0800

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,50 @@
-module.exports = {
-	entry: [
-        'webpack/hot/dev-server',
-        './app/main.jsx'
-    ],
+module.exports = function(env) {
+	env = env || 'dev';
+	var isDev = env === 'dev';
 
-	output: {
-		path: './build',
-		filename: 'main.js'
-	},
+	var conf = {
+		entry: isDev ? ['webpack/hot/dev-server', './app/main.jsx'] : './app/main.jsx',
 
-    externals: {
-        "react": "React"
-    },
+		output: {
+			path: './dist',
+			filename: 'main.js'
+		},
 
-	module: {
-		loaders: [
-			{test: /\.jsx$/, loader: 'jsx-loader?harmony'},
-            {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'}
-		]
-	}
+		resolve: {
+	    extensions: ['', '.js', '.jsx', '.scss']
+	  },
+
+		externals: [
+			/^react(\/.*)?$/,
+			/^reflux(\/.*)?$/,
+			"superagent"
+		],
+
+		plugins: [new webpack.PrefetchPlugin("react")],
+
+		module: {
+			loaders: [
+				{
+					test: /\.(png|jpg|jpeg|gif|svg)$/,
+					loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+				},
+
+				{
+					test: /\.jsx$/,
+					loader: ["react-hot-loader", "babel-loader?stage=0"],
+					exclude: /(node_modules|bower_components)/
+				},
+
+				{
+					test: /\.scss$/,
+					loader: 'style-loader!css-loader?module' + (isDev ? '' : '&localIdentName=[path][name]---[local]---[hash:base64:5]') + '!sass-loader'
+				},
+
+				{
+					test: /\.html/,
+					loader: 'html-loader'
+				}
+			]
+		}
+	};
 };
